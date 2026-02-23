0ad76c3ff6e136d499c9fabbf5db5d6fbba27f06
Timothee Pablo Mege <tmege@cbr15s4.42barcelona.com>
2026-02-23 18:07:28 +0100

    Y

--- src/executor_pipe.c
+++ src/executor_pipe.c
@@ -12,7 +12,7 @@
 
 #include "minishell.h"
 
-/* fds[0]=prev_read  fds[1]=new_pipe_read  fds[2]=new_pipe_write */
+/* st[0]=prev_read st[1]=pipe_read st[2]=pipe_write st[3]=i st[4]=n */
 
 static void	pipe_child_setup(int *fds, int is_last)
 {
@@ -58,19 +58,6 @@ static void	pipe_child(t_cmd *cmd, t_data *data, int *fds, int is_last)
 	pipe_exec_cmd(cmd, data);
 }
 
-static void	pipe_parent_update(int *fds, int is_last)
-{
-	if (fds[0] != -1)
-		close(fds[0]);
-	if (!is_last)
-	{
-		close(fds[2]);
-		fds[0] = fds[1];
-	}
-	else
-		fds[0] = -1;
-}
-
 static void	wait_children(pid_t *pids, int n, t_data *data)
 {
 	int	i;
@@ -97,34 +84,48 @@ static void	wait_children(pid_t *pids, int n, t_data *data)
 	}
 }
 
+static int	pipe_fork_one(t_cmd **cmd, t_data *data, int *st, pid_t *pids)
+{
+	if (st[3] < st[4] - 1 && pipe(st + 1) < 0)
+	{
+		perror("minishell");
+		return (-1);
+	}
+	pids[st[3]] = fork();
+	if (pids[st[3]] == 0)
+		pipe_child(*cmd, data, st, st[3] == st[4] - 1);
+	if (st[0] != -1)
+		close(st[0]);
+	if (st[3] < st[4] - 1)
+	{
+		close(st[2]);
+		st[0] = st[1];
+	}
+	else
+		st[0] = -1;
+	*cmd = (*cmd)->next;
+	return (0);
+}
+
 void	execute_pipeline(t_data *data)
 {
 	pid_t	*pids;
 	t_cmd	*cmd;
-	int		fds[3];
-	int		info[2];
+	int		st[5];
 
-	info[1] = count_cmds(data->cmds);
-	pids = malloc(sizeof(pid_t) * info[1]);
+	st[4] = count_cmds(data->cmds);
+	pids = malloc(sizeof(pid_t) * st[4]);
 	if (!pids)
 		return ;
 	setup_signals_exec();
-	fds[0] = -1;
+	st[0] = -1;
 	cmd = data->cmds;
-	info[0] = -1;
-	while (++info[0] < info[1])
+	st[3] = -1;
+	while (++st[3] < st[4])
 	{
-		if (info[0] < info[1] - 1 && pipe(fds + 1) < 0)
-		{
-			perror("minishell");
+		if (pipe_fork_one(&cmd, data, st, pids) < 0)
 			break ;
-		}
-		pids[info[0]] = fork();
-		if (pids[info[0]] == 0)
-			pipe_child(cmd, data, fds, info[0] == info[1] - 1);
-		pipe_parent_update(fds, info[0] == info[1] - 1);
-		cmd = cmd->next;
 	}
-	wait_children(pids, info[1], data);
+	wait_children(pids, st[4], data);
 	free(pids);
 }
--- src/expander_tokens.c
+++ src/expander_tokens.c
@@ -6,53 +6,92 @@
 /*   By: tmege <tmege@student.42barcelona.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 13:00:35 by tmege             #+#    #+#             */
-/*   Updated: 2026/02/16 14:32:18 by tmege            ###   ########.fr       */
+/*   Updated: 2026/02/23 00:00:00 by tmege            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void	remove_token(t_token **tokens, t_token *prev, t_token *cur)
+static int	is_ifs(char c)
 {
-	if (prev)
-		prev->next = cur->next;
-	else
-		*tokens = cur->next;
-	free(cur->value);
-	free(cur);
+	return (c == ' ' || c == '\t' || c == '\n');
 }
 
-static int	try_remove(t_token **tokens, t_token **prev, t_token **cur)
+static t_token	*make_word_tokens(char *value)
 {
-	if (!(*cur)->quoted && (*cur)->value[0] == '\0')
+	t_token	*head;
+	char	*word;
+	int		i;
+	int		start;
+
+	head = NULL;
+	i = 0;
+	while (value[i])
 	{
-		remove_token(tokens, *prev, *cur);
-		if (*prev)
-			*cur = (*prev)->next;
-		else
-			*cur = *tokens;
-		return (1);
+		while (value[i] && is_ifs(value[i]))
+			i++;
+		if (!value[i])
+			break ;
+		start = i;
+		while (value[i] && !is_ifs(value[i]))
+			i++;
+		word = ft_substr(value, start, i - start);
+		token_add_back(&head, token_new(word, token_word, 0));
 	}
-	return (0);
+	return (head);
+}
+
+static t_token	*do_word_split(t_token **tokens, t_token *prev,
+	t_token *cur, t_token *next)
+{
+	t_token	*split;
+	t_token	*last;
+
+	split = make_word_tokens(cur->value);
+	free(cur->value);
+	free(cur);
+	if (!split)
+	{
+		if (prev)
+			prev->next = next;
+		else
+			*tokens = next;
+		return (prev);
+	}
+	last = split;
+	while (last->next)
+		last = last->next;
+	last->next = next;
+	if (prev)
+		prev->next = split;
+	else
+		*tokens = split;
+	return (last);
 }
 
 void	expand_tokens(t_token **tokens, t_data *data)
 {
 	t_token	*cur;
 	t_token	*prev;
+	t_token	*next;
 	char	*expanded;
 
 	prev = NULL;
 	cur = *tokens;
 	while (cur)
 	{
+		next = cur->next;
 		if (cur->type == token_word)
 		{
 			expanded = expand_value(cur->value, data, cur->quoted);
 			free(cur->value);
 			cur->value = expanded;
-			if (try_remove(tokens, &prev, &cur))
+			if (!cur->quoted)
+			{
+				prev = do_word_split(tokens, prev, cur, next);
+				cur = next;
 				continue ;
+			}
 		}
 		prev = cur;
 		cur = cur->next;
